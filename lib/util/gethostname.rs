6a9b17ed2f1e48fec719ad4ec78a61863b50faae
binlingyu <binlingyu@uniontech.com>
2023-06-14 13:53:52 +0800

    修改gethostname.rs文件：增加sudo_gethostname_v1函数

---
+++ lib/util/gethostname.rs
@@ -0,0 +1,26 @@
+#[no_mangle]
+pub unsafe extern "C" fn sudo_gethostname_v1() -> *mut libc::c_char {
+    let mut hname: *mut libc::c_char = 0 as *mut libc::c_char;
+    let mut host_name_max: size_t = 0;
+
+    host_name_max = sysconf(_SC_HOST_NAME_MAX as libc::c_int) as size_t;
+    if host_name_max == -(1 as libc::c_int) as size_t {
+        host_name_max = 255 as libc::c_int as size_t;
+    }
+    hname =
+        malloc(host_name_max.wrapping_add(1 as libc::c_int as libc::c_ulong)) as *mut libc::c_char;
+    if !hname.is_null() {
+        if gethostname(
+            hname,
+            host_name_max.wrapping_add(1 as libc::c_int as libc::c_ulong),
+        ) == 0 as libc::c_int
+            && *hname as libc::c_int != '\u{0}' as i32
+        {
+            *hname.offset(host_name_max as isize) = '\u{0}' as i32 as libc::c_char;
+        } else {
+            free(hname as *mut libc::c_void);
+            hname = 0 as *mut libc::c_char;
+        }
+    }
+    return hname;
+}
