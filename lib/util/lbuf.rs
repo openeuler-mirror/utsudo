ab66c21d355d62299afc2f8ed072f5e917130f25
houlifei <houlifei@uniontech.com>
2023-06-15 18:58:56 +0800

    删除：lbuf.c文件，增加：lbuf.rs文件并且添加类型属性内容
    Signed-off-by: houlifei <houlifei@uniontech.com>

---
+++ lib/util/lbuf.rs
@@ -0,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+#![allow(
+    non_camel_case_types,
+    non_upper_case_globals,
+    unused_assignments,
+    unused_mut,
+    dead_code
+)]
+
+
