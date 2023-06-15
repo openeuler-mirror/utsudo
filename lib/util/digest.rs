56002fd379ee88e692993001eb230fc2006b1732
zhangxingrong <zhangxingrong@uniontech.com>
2023-06-15 18:35:16 +0800

    修改digest.rs文件,增加digest_function结构体

--- lib/util/digest.rs
+++ lib/util/digest.rs
@@ -4,6 +4,28 @@
  * SPDX-License-Identifier: MulanPSL-2.0
  */
 
+//call other file's func
+use crate::sha2::sudo_SHA224Final;
+use crate::sha2::sudo_SHA224Init;
+use crate::sha2::sudo_SHA224Update;
+use crate::sha2::sudo_SHA256Final;
+use crate::sha2::sudo_SHA256Init;
+use crate::sha2::sudo_SHA256Update;
+use crate::sha2::sudo_SHA384Final;
+use crate::sha2::sudo_SHA384Init;
+use crate::sha2::sudo_SHA384Update;
+use crate::sha2::sudo_SHA512Final;
+use crate::sha2::sudo_SHA512Init;
+use crate::sha2::sudo_SHA512Update;
+use crate::sha2::SHA2_CTX;
+
+pub struct digest_function {
+    pub init: Option<unsafe extern "C" fn(*mut SHA2_CTX) -> ()>,
+    pub update: Option<unsafe extern "C" fn(*mut SHA2_CTX, *const libc::c_uchar, size_t) -> ()>,
+    pub final_0: Option<unsafe extern "C" fn(*mut libc::c_uchar, *mut SHA2_CTX) -> ()>,
+}
+
+
 #[no_mangle]
 unsafe fn sudo_digest_free_v1() {
     debug_return!()
