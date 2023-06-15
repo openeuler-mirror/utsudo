4d85297ac1a29f8693e7a9e4a2ca738cef5d1efd
binlingyu <binlingyu@uniontech.com>
2023-06-15 11:18:47 +0800

    修改event.rs文件：增加sudo_ev_deactivate_all函数

--- lib/util/event.rs
+++ lib/util/event.rs
@@ -9,3 +9,15 @@ pub unsafe extern "C" fn sudo_ev_activate(mut base: *mut sudo_event_base, mut ev
     (*base).active.tqh_last = &mut (*ev).active_entries.tqe_next;
     (*ev).flags = ((*ev).flags as libc::c_int | 0x2 as libc::c_int) as libc::c_short;
 }
+
+#[inline]
+unsafe extern "C" fn sudo_ev_deactivate(mut base: *mut sudo_event_base, mut ev: *mut sudo_event) {
+    (*ev).flags = ((*ev).flags as libc::c_int & !(0x2 as libc::c_int)) as libc::c_short;
+    if !((*ev).active_entries.tqe_next).is_null() {
+        (*(*ev).active_entries.tqe_next).active_entries.tqe_prev = (*ev).active_entries.tqe_prev;
+    } else {
+        (*base).active.tqh_last = (*ev).active_entries.tqe_prev;
+    }
+    *(*ev).active_entries.tqe_prev = (*ev).active_entries.tqe_next;
+}
+
