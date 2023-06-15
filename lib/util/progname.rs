8cc78868e04d9ddf48ef6c2651babcd24b3ed30b
binlingyu <binlingyu@uniontech.com>
2023-06-15 14:08:23 +0800

    修改progname.rs文件：增加initprogname函数

--- lib/util/progname.rs
+++ lib/util/progname.rs
@@ -1,4 +1,9 @@
 #[no_mangle]
+pub unsafe extern "C" fn initprogname(mut name: *const libc::c_char) {
+
+}
+
+#[no_mangle]
 pub unsafe extern "C" fn sudo_getprogname() -> *const libc::c_char {
     return progname;
 }
