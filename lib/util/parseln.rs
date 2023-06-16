07834fd2fbacb3cda0f9897d58fe09d9f2dfdde1
litianze <litianze@uniontech.com>
2023-06-16 13:49:02 +0800

    增加 _IO_FILE结构体/ 增加全局变量

--- lib/util/parseln.rs
+++ lib/util/parseln.rs
@@ -4,6 +4,14 @@
  * SPDX-License-Identifier: MulanPSL-2.0
  */
 
+
+use crate::sudo_debug::sudo_debug_enter_v1;
+use crate::sudo_debug::sudo_debug_exit_ssize_t_v1;
+use crate::sudo_debug::sudo_debug_printf2_v1;
+use crate::sudo_debug_macro::SUDO_DEBUG_ERROR;
+use crate::sudo_debug_macro::SUDO_DEBUG_LINENO;
+use crate::sudo_debug_macro::SUDO_DEBUG_UTIL;
+
 pub struct _IO_marker {
     _unused: [u8; 0],
 }
@@ -22,6 +30,17 @@ pub type __off_t = libc::c_long;
 pub type __off64_t = libc::c_long;
 
 pub struct _IO_FILE {
+    pub _IO_read_ptr: *mut libc::c_char,
+    pub _IO_read_end: *mut libc::c_char,
+    pub _IO_read_base: *mut libc::c_char,
+    pub _IO_write_base: *mut libc::c_char,
+    pub _IO_write_ptr: *mut libc::c_char,
+    pub _IO_write_end: *mut libc::c_char,
+    pub _IO_buf_base: *mut libc::c_char,
+    pub _IO_buf_end: *mut libc::c_char,
+    pub _IO_save_base: *mut libc::c_char,
+    pub _IO_backup_base: *mut libc::c_char,
+    pub _IO_save_end: *mut libc::c_char,
     pub _unused2: [libc::c_char; 20],
 }
 
