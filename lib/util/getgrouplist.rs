58806186a7179256aebd1bfe55a47d4e9bdf87bf
yangfei <yangfei@uniontech.com>
2023-06-16 17:34:47 +0800

    getgroupplist.rs导入c函数

--- lib/util/getgrouplist.rs
+++ lib/util/getgrouplist.rs
@@ -3,10 +3,24 @@
  *
  * SPDX-License-Identifier: MulanPSL-2.0
  */
- 
+
  #![allow(non_camel_case_types, unused_mut, unused_variables, unused_assignments)]
 
  //line72-2(arg),
 pub type gid_t = libc::c_uint;
 pub type __gid_t = libc::c_uint;
 pub type size_t = libc::c_ulong;
+
+extern "C" {
+	fn getgrouplist(
+	    __user: *const libc::c_char,
+	    __group: __gid_t,
+	    __groups: *mut __gid_t,
+	    __ngroups: *mut libc::c_int,
+	) -> libc::c_int;
+	fn sysconf(__name: libc::c_int) -> libc::c_long;
+	fn free(__ptr: *mut libc::c_void);
+	fn reallocarray(__ptr: *mut libc::c_void, __nmemb: size_t, __size: size_t)
+	    -> *mut libc::c_void;
+    }
+    
