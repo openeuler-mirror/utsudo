9e1c1667c79b71dd5f8eb40ff338432dc90ed9e7
litianze <litianze@uniontech.com>
2023-06-16 15:17:39 +0800

    增加文件setgroups.rs/ 设定其主要函数名

---
+++ lib/util/setgroups.rs
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+
+use crate::sudo_debug::sudo_debug_enter_v1;
+use crate::sudo_debug::sudo_debug_exit_int_v1;
+
+
+unsafe extern "C" fn sudo_setgroups_v1()
+    
