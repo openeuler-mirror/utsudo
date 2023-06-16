253d46a2a7854f2b6f19eb1c296c06ca2a94ac6e
houlifei <houlifei@uniontech.com>
2023-06-16 14:02:10 +0800

    修改sudo_debug.rs文件，定义公有的数据类型别名
    Signed-off-by: houlifei <houlifei@uniontech.com>

--- lib/util/sudo_debug.rs
+++ lib/util/sudo_debug.rs
@@ -13,3 +13,16 @@
     unused_assignments,
     unused_mut
 )]
+
+use crate::S_IWUSR;
+
+pub type size_t = usize;
+pub type __pid_t = libc::c_int;
+pub type pid_t = __pid_t;
+pub type __time_t = libc::c_long;
+pub type time_t = __time_t;
+pub type __ssize_t = libc::c_long;
+pub type ssize_t = __ssize_t;
+pub type __id_t = libc::c_uint;
+pub type id_t = __id_t;
+
