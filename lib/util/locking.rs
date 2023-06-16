dd4036cf153ab70b1bc1a4813f2874be8879398a
litianze <litianze@uniontech.com>
2023-06-16 10:56:31 +0800

    增加文件locking.rs/ 设定其主要函数名

---
+++ lib/util/locking.rs
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+#![allow(dead_code, non_camel_case_types, unused_mut)]
+
+use crate::sudo_debug::sudo_debug_enter_v1;
+use crate::sudo_debug::sudo_debug_exit_bool_v1;
+
+unsafe extern "C" fn lockf()
+
+unsafe extern "C" fn sudo_lock_file_v1()
+
+unsafe extern "C" fn sudo_lock_region_v1()
+
