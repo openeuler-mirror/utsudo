ad7bd715ee8b2188eeaa281f49abc5a3f486936b
houlifei <houlifei@uniontech.com>
2023-06-16 14:38:34 +0800

    修改gidlist.rs文件, 添加extern C 外部调用函数
    Signed-off-by: houlifei <houlifei@uniontech.com>

--- lib/util/gidlist.rs
+++ lib/util/gidlist.rs
@@ -15,3 +15,9 @@ use crate::sudo_debug::sudo_debug_enter_v1;
 use crate::sudo_debug::sudo_debug_exit_int_v1;
 use crate::sudo_debug_macro::SUDO_DEBUG_UTIL;
 
+extern "C" {
+    fn reallocarray(__ptr: *mut libc::c_void, __nmemb: size_t, __size: size_t)
+        -> *mut libc::c_void;
+    fn free(__ptr: *mut libc::c_void);
+    fn sudo_warnx_nodebug_v1(fmt: *const libc::c_char, _: ...);
+}
