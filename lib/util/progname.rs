50aa05a8aae321a69dbbb9b86181349c68772373
binlingyu <binlingyu@uniontech.com>
2023-06-16 13:41:09 +0800

    修改progname.rs文件：给initprogname函数增加一个if判断

--- lib/util/progname.rs
+++ lib/util/progname.rs
@@ -1,9 +1,19 @@
 #[no_mangle]
 pub unsafe extern "C" fn initprogname(mut name: *const libc::c_char) {
-
+    if !__progname.is_null() && *__progname as libc::c_int != '\u{0}' as i32 {
+        progname = __progname;
+    } else {
+        progname = strrchr(name, '/' as i32);
+        if !progname.is_null() {
+            progname = progname.offset(1); //progname++
+        } else {
+            progname = name;
+        }
+    }
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn sudo_getprogname() -> *const libc::c_char {
     return progname;
 }
+
