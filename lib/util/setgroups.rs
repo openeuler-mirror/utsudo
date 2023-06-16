68c1666e58734d508e068734011c129f522ffd87
litianze <litianze@uniontech.com>
2023-06-16 15:55:16 +0800

    增加注释

--- lib/util/setgroups.rs
+++ lib/util/setgroups.rs
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: MulanPSL-2.0
  */
 
+//allow
 
 extern "C" {
     fn sysconf(__name: libc::c_int) -> libc::c_long;
@@ -11,28 +12,34 @@ extern "C" {
     fn setgroups(__n: size_t, __groups: *const gid_t) -> libc::c_int;
 }
 
+//func be included
 use crate::sudo_debug::sudo_debug_enter_v1;
 use crate::sudo_debug::sudo_debug_exit_int_v1;
 use crate::sudo_debug_macro::SUDO_DEBUG_UTIL;
-
+ 
 pub type gid_t = libc::c_uint;
 pub type size_t = libc::c_ulong;
 
+//define
 pub const EINVAL: libc::c_int = 22;
 pub const _SC_NGROUPS_MAX: libc::c_int = 3;
 pub const NGROUPS_MAX: libc::c_int = 65536;
 
+//func sudo_setgroups_v1
 unsafe extern "C" fn sudo_setgroups_v1(
     mut ngids: libc::c_int,
     mut gids: *const gid_t,
-) -> libc::c_int {
-    let mut maxgids: libc::c_int = 0;
+@@ -30,9 +34,13 @@ unsafe extern "C" fn sudo_setgroups_v1(
     let mut ret: libc::c_int = 0;
 
     debug_decl!(stdext::function_name!().as_ptr(), SUDO_DEBUG_UTIL);
+
+    //line43
     ret = setgroups(ngids as size_t, gids as *mut gid_t);
     
+    //line44
     if *__errno_location() == EINVAL as libc::c_int {
+
     //max
     }
 }
