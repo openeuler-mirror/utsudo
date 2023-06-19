e5bcb626b95093cdbd3aba8c342a29299838ca4d
litianze <litianze@uniontech.com>
2023-06-19 13:40:28 +0800

    To-initiate-strsplit-and-define-main-funcs

---
+++ lib/util/strsplit.rs
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+use crate::sudo_debug::sudo_debug_enter_v1;
+use crate::sudo_debug::sudo_debug_exit_ptr_v1;
+use crate::sudo_debug_macro::SUDO_DEBUG_UTIL;
+
+unsafe fn sudo_strsplit_v1()
