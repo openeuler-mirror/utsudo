e306f6dc0eac0430baf8bb233842f82177854ecf
litianze <litianze@uniontech.com>
2023-06-19 13:32:41 +0800

    To-initiate-sig2str-and-define-main-funcs

---
+++ lib/util/sig2str.rs
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+pub type size_t = libc::c_ulong;
+pub type __int32_t = libc::c_int;
+
+pub unsafe extern "C" fn toupper()
+
+pub unsafe extern "C" fn sudo_sig2str()
+
