90ebc607444ce1f28326bcd6a5886123d9330dfa
houlifei <houlifei@uniontech.com>
2023-06-19 17:40:36 +0800

    Declare C external function
    Signed-off-by: houlifei <houlifei@uniontech.com>

--- lib/util/key_val.rs
+++ lib/util/key_val.rs
@@ -10,3 +10,10 @@ use crate::sudo_debug::sudo_debug_enter_v1;
 use crate::sudo_debug::sudo_debug_exit_str_v1;
 use crate::sudo_debug_macro::SUDO_DEBUG_UTIL;
 
+extern "C" {
+    fn malloc(_: libc::c_ulong) -> *mut libc::c_void;
+    fn memcpy(_: *mut libc::c_void, _: *const libc::c_void, _: libc::c_ulong) -> *mut libc::c_void;
+    fn strlen(_: *const libc::c_char) -> libc::c_ulong;
+}
+pub type size_t = libc::c_ulong;
+
