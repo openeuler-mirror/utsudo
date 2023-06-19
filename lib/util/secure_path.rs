e3281736e6a7e0111b1d75f37bf3d50bbb6b0858
binlingyu <binlingyu@uniontech.com>
2023-06-19 15:12:34 +0800

    修改strtonum.rs文件，增加stat函数

--- lib/util/secure_path.rs
+++ lib/util/secure_path.rs
@@ -3,3 +3,12 @@
  *
  * SPDX-License-Identifier: MulanPSL-2.0
  */
+
+
+#[inline]
+unsafe extern "C" fn stat(
+    mut __path: *const libc::c_char,
+    mut __statbuf: *mut stat,
+) -> libc::c_int {
+    return __xstat(1 as libc::c_int, __path, __statbuf);
+}
