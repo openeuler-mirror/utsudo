417940af3d0a5f9d6a809a30c0bf24a2ba7ba6d9
binlingyu <binlingyu@uniontech.com>
2023-06-20 10:20:45 +0800

    Realize the function of the sudo_secure_path function

--- lib/util/secure_path.rs
+++ lib/util/secure_path.rs
@@ -80,4 +80,58 @@ pub unsafe extern "C" fn sudo_secure_path(
     mut uid: uid_t,
     mut gid: gid_t,
     mut sbp: *mut stat,
-) -> libc::c_int {}
+) -> libc::c_int {
+    let mut sb: stat = stat {
+        st_dev: 0,
+        st_ino: 0,
+        st_nlink: 0,
+        st_mode: 0,
+        st_uid: 0,
+        st_gid: 0,
+        __pad0: 0,
+        st_rdev: 0,
+        st_size: 0,
+        st_blksize: 0,
+        st_blocks: 0,
+        st_atim: timespec {
+            tv_sec: 0,
+            tv_nsec: 0,
+        },
+        st_mtim: timespec {
+            tv_sec: 0,
+            tv_nsec: 0,
+        },
+        st_ctim: timespec {
+            tv_sec: 0,
+            tv_nsec: 0,
+        },
+        __glibc_reserved: [0; 3],
+    };
+    let mut ret: libc::c_int = SUDO_PATH_MISSING!();
+    debug_decl!(stdext::function_name!().as_ptr(), SUDO_DEBUG_UTIL);
+
+    if (!path.is_null()) && (stat(path, &mut sb) == 0) {
+        if (sb.st_mode & _S_IFMT!()) != c_type {
+            ret = SUDO_PATH_BAD_TYPE!();
+        } else if uid != -(1 as libc::c_int) as uid_t && sb.st_uid != uid {
+            ret = SUDO_PATH_WRONG_OWNER!();
+        } else if (sb.st_mode & S_IWOTH!()) != 0 {
+            ret = SUDO_PATH_WORLD_WRITABLE!();
+        } else if ISSET!(sb.st_mode, S_IWGRP!()) != 0
+            && (gid == -(1 as libc::c_int) as gid_t || sb.st_gid != gid)
+        {
+            ret = SUDO_PATH_GROUP_WRITABLE!();
+        } else {
+            ret = SUDO_PATH_SECURE!();
+        }
+        if !sbp.is_null() {
+            memcpy(
+                sbp as *mut libc::c_void,
+                &mut sb as *mut stat as *const libc::c_void,
+                ::std::mem::size_of::<stat>() as libc::c_ulong,
+            );
+        }
+    }
+
+    debug_return_int!(ret)
+}
