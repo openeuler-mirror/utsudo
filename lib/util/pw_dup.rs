45ee69752ccbc1db7b88e823540bf7ab350bc9c2
zhangxingrong <zhangxingrong@uniontech.com>
2023-06-20 11:12:22 +0800

    delete pw_dup.c and add pw_dup.rs file

---
+++ lib/util/pw_dup.rs
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+#![allow(
+    mutable_transmutes,
+    non_camel_case_types,
+    unused_assignments,
+    unused_mut,
+    clashing_extern_declarations,
+    dead_code,
+    unused_variables,
+    unused_macros
+)]
