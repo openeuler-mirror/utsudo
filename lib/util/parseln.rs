bdde212730aa3d39d75062516e440f69fccc90e5
litianze <litianze@uniontech.com>
2023-06-20 14:54:16 +0800

    To-get-in-external-C-funcs

--- lib/util/parseln.rs
+++ lib/util/parseln.rs
@@ -32,6 +32,14 @@ pub struct _IO_wide_data {
     _unused: [u8; 0],
 }
 
+extern "C" {
+    fn strchr()
+    fn __ctype_b_loc()
+    fn realloc()
+    fn free()
+    fn memcpy()
+    fn sudo_debug_printf2_v1()
+}
 
 pub type __SIZE_TYPE__ = libc::c_ulong;
 pub type size_t = __SIZE_TYPE__;
