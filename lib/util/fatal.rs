b7b7815d0f374bc8dc3174477f081a9995d1e3c1
yangfei <yangfei@uniontech.com>
2023-06-21 16:48:50 +0800

    fatal.rs define SUDO_CONV_PROMPT_MASK

--- lib/util/fatal.rs
+++ lib/util/fatal.rs
@@ -78,6 +78,7 @@ pub const SUDO_CONV_PROMPT_ECHO_OFF: libc::c_int = 1;
 pub const SUDO_CONV_PROMPT_ECHO_ON: libc::c_int = 2;
 pub const SUDO_CONV_ERROR_MSG: libc::c_int = 3;
 pub const SUDO_CONV_INFO_MSG: libc::c_int = 4;
+pub const SUDO_CONV_PROMPT_MASK: libc::c_int = 5;
 
 #[derive(Copy, Clone)]
 pub struct sudo_conv_message {
