6a5f812f98c978fb095319e6bd1aa3886740a8e5
litianze <litianze@uniontech.com>
2023-06-21 13:42:42 +0800

    To-add-return-value-and-mut-vars-under-sudo_strsplit_v1

--- lib/util/strsplit.rs
+++ lib/util/strsplit.rs
@@ -10,4 +10,8 @@ use crate::sudo_debug::sudo_debug_enter_v1;
 use crate::sudo_debug::sudo_debug_exit_ptr_v1;
 use crate::sudo_debug_macro::SUDO_DEBUG_UTIL;
 
-unsafe fn sudo_strsplit_v1()
+unsafe fn sudo_strsplit_v1(
+    mut str: *const libc::c_char,
+    mut last: *mut *const libc::c_char,
+) -> *const libc::c_char {
+}
