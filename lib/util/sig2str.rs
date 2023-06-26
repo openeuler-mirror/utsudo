e4634d47b92bbdb5fe0b34f67af110fd7219e341
litianze <litianze@uniontech.com>
2023-06-26 18:14:21 +0800

    To-define-functions-vars-and-return-value

--- lib/util/sig2str.rs
+++ lib/util/sig2str.rs
@@ -4,10 +4,40 @@
  * SPDX-License-Identifier: MulanPSL-2.0
  */
 
+#![allow(
+    dead_code,
+    mutable_transmutes,
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
+    unused_assignments,
+    unused_mut
+)]
+
 pub type size_t = libc::c_ulong;
 pub type __int32_t = libc::c_int;
 
-pub unsafe extern "C" fn toupper()
+extern "C" {
+    fn __errno_location()
+    fn __libc_current_sigrtmin()
+    fn __libc_current_sigrtmax()
+    fn sysconf()
+    fn snprintf()
+    fn sudo_strlcpy()
+    fn __ctype_toupper_loc()
+    fn __ctype_b_loc()
+}
 
-pub unsafe extern "C" fn sudo_sig2str()
+pub unsafe extern "C" fn toupper() -> libc::c_int {
+    return libc::c_int {
+        *(*__ctype_toupper_loc()).offset(__c as isize)
+    };
+}
+
+pub unsafe extern "C" fn sudo_sig2str(
+    signo: libc::c_int,
+    signame: *mut libc::c_char,
+) -> libc::c_int {
+    return 0;
+}
 
