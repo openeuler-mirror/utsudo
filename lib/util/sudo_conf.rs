115f6de97041ab3827dcbf022df7d798cf84471b
zhangxingrong <zhangxingrong@uniontech.com>
2023-06-27 14:37:32 +0800

    modify sudo_conf.rs:Realize the function of parse_variable

--- lib/util/sudo_conf.rs
+++ lib/util/sudo_conf.rs
@@ -26,5 +26,34 @@ pub unsafe extern "C" fn parse_variable(
     mut conf_file: *const libc::c_char,
     mut lineno: libc::c_uint,
 ) -> libc::c_int {
-    debug_return_int!(0);
+    let mut var: *mut sudo_conf_table = 0 as *mut sudo_conf_table;
+    let mut ret: libc::c_int = 0;
+
+    debug_decl!(stdext::function_name!().as_ptr(), SUDO_DEBUG_UTIL);
+
+    var = sudo_conf_var_table.as_mut_ptr();
+    while !((*var).name).is_null() {
+        if strncmp(entry, (*var).name, (*var).namelen as libc::c_ulong) == 0
+            && isblank!(entry.offset((*var).namelen as isize)) != 0
+        {
+            entry = entry.offset(((*var).namelen + 1) as isize);
+
+            while isblank!(*entry) != 0 {
+                entry = entry.offset(1 as isize);
+            }
+
+            ret = ((*var).parser).expect("non-null function pointer")(entry, conf_file, lineno);
+
+            if ret != 0 {
+                ret = SUDO_DEBUG_INFO;
+            } else {
+                ret = SUDO_DEBUG_ERROR;
+            }
+
+            debug_return_int!(ret);
+        }
+        var = var.offset(1);
+    } // while !((*var).name).is_null()
+
+    debug_return_int!(false as libc::c_int);
 }
