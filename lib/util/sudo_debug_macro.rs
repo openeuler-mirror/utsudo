01840c9fda72708edb987c5f626426ff2b7dd943
bianguangze <bianguangze@uniontech.com>
2023-07-04 10:59:34 +0800

    add macro_debug_return_id_t

--- lib/util/sudo_debug_macro.rs
+++ lib/util/sudo_debug_macro.rs
@@ -120,8 +120,20 @@ macro_rules! debug_return_int {
 }
 
 
-
-
+// 完成
+#[macro_export]
+macro_rules! debug_return_id_t {
+    ($ret:expr) => {{
+        sudo_debug_exit_id_t_v1(
+            stdext::function_name!().as_ptr() as *const libc::c_char,
+            file!().as_ptr() as *const libc::c_char,
+            line!() as libc::c_int,
+            sudo_debug_subsys as libc::c_int,
+            $ret,
+        );
+        return $ret;
+    }};
+}
 
 
 
