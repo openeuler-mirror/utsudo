86d69facbb738aeb4d61413477a46af2ef0fd8b5
litianze <litianze@uniontech.com>
2023-07-04 10:38:15 +0800

    To add comments and tags

--- lib/util/term.rs
+++ lib/util/term.rs
@@ -36,38 +36,53 @@ pub struct __dso_public {
     pub sudo_term_kill = libc::c_int,
 }
 
+#[no_mangle]
 pub static mut sudo_term_eof: libc::c_int = 0;
+#[no_mangle]
 pub static mut sudo_term_erase: libc::c_int = 0;
+#[no_mangle]
 pub static mut sudo_term_kill: libc::c_int = 0;
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub union sigval {
     pub sival_int: libc::c_int,
     pub sival_ptr: *mut libc::c_void,
 }
 pub type __sigval_t = sigval;
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct siginfo_t {
     pub si_signo: libc::c_int,
     pub si_errno: libc::c_int,
     pub si_code: libc::c_int,
 }
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct kill_struct {
     pub si_pid: __pid_t,
     pub si_uid: __uid_t,
 }
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct timer_struct {
     pub si_tid: libc::c_int,
     pub si_sigval: __sigval_t,
 }
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct rt_struct {
     pub si_pid: __pid_t,
     pub si_uid: __uid_t,
     pub si_sigval: __sigval_t,
 }
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct sigchld_struct {
     pub si_pid: __pid_t,
     pub si_uid: __uid_t,
@@ -76,23 +91,31 @@ pub struct sigchld_struct {
     pub si_stime: __clock_t,
 }
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct sigfault_struct {
     pub si_addr: *mut libc::c_void,
     pub si_addr_lsb: libc::c_short,
     pub _bounds: bounds_struct,
 }
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct sigpoll_struct {
     pub si_band: libc::c_long,
     pub si_fd: libc::c_int,
 }
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct sigsys_struct {
     pub _call_addr: *mut libc::c_void,
     pub _syscall: libc::c_int,
     pub _arch: libc::c_uint,
 }
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub union sifields_union {
     pub _kill: kill_struct,
     pub _timer: timer_struct,
@@ -102,30 +125,56 @@ pub union sifields_union {
     pub _sigsys: sigsys_struct,
 }
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct bounds_struct {
     pub _addr_bnd: addr_bnd_struct,
     pub _key: __uint32_t,
 }
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct addr_bnd_struct {
     pub _lower: *mut libc::c_void,
     pub _upper: *mut libc::c_void,
 }
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct __sigset_t {
     pub __val: [libc::c_ulong; 16],
 }
 pub type sigset_t = __sigset_t;
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub union __sigaction_handler_union {
 }
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct sigaction {
     pub __sigaction_handler: __sigaction_handler_union,
+
+    /* Additional set of signals to be blocked.  */
     pub sa_mask: sigset_t,
+
+    /* Special flags.  */
     pub sa_flags: libc::c_int,
+
+    /* Restore handler.  */
+    // void (*sa_restorer) (void);
     pub sa_restorer: Option<unsafe extern "C" fn() -> ()>,
 }
+
+/* An integral type that can be modified atomically, without the
+possibility of a signal arriving in the middle of the operation.  */
+//    typedef __sig_atomic_t sig_atomic_t;
+
+/* C99: An integer type that can be accessed as an atomic entity,
+even in the presence of asynchronous interrupts.
+It is not currently necessary for this to be machine-specific.  */
+// typedef int __sig_atomic_t;
 
 pub type sig_atomic_t = libc::c_int;
 static mut got_sigttou: sig_atomic_t = 0 as sig_atomic_t;
@@ -151,6 +200,8 @@ static mut oterm: termios = termios {
     c_ospeed: 0,
 };
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct termios {
     c_iflag: tcflag_t, 
     c_oflag: tcflag_t, 
@@ -162,6 +213,8 @@ pub struct termios {
     c_ospeed: speed_t, 
 }
 
+#[derive(Copy, Clone)]
+#[repr(C)]
 pub struct winsize {
     ws_row: libc::c_ushort,
     ws_col: libc::c_ushort,
