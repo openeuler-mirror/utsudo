6711576002ddd50246f06743770c9e89601f4a1f
zhangxingrong <zhangxingrong@uniontech.com>
2023-07-05 10:46:28 +0800

    modify sudo_conf.rs:Supplement the content of run_static_initializers function

--- lib/util/sudo_conf.rs
+++ lib/util/sudo_conf.rs
@@ -876,5 +876,22 @@ unsafe extern "C" fn run_static_initializers() {
             };
             init
         },
+        {
+            let mut init = sudo_conf_table {
+                name: b"Set\0" as *const u8 as *const libc::c_char,
+                namelen: (::std::mem::size_of::<[libc::c_char; 4]>() as libc::c_ulong)
+                    .wrapping_sub(1 as libc::c_int as libc::c_ulong)
+                    as libc::c_uint,
+                parser: Some(
+                    parse_variable
+                        as unsafe extern "C" fn(
+                            *const libc::c_char,
+                            *const libc::c_char,
+                            libc::c_uint,
+                        ) -> libc::c_int,
+                ),
+            };
+            init
+        },
     ]
 }
