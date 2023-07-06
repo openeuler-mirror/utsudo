d7bccff97abe4ce1097682f449477e4992af9b7c
litianze <litianze@uniontech.com>
2023-07-06 11:29:52 +0800

    To add in condition: count the line length -1 when it ends by a \r

--- lib/util/parseln.rs
+++ lib/util/parseln.rs
@@ -132,7 +132,8 @@ pub unsafe extern "C" fn sudo_parseln_v2(
             *lineno = (*lineno).wrapping_add(1) //(*lineno)++
         }
         while len > 0 as libc::c_long
-            && (*line.offset((len - 1 as libc::c_long) as isize) as libc::c_int == '\n' as i32)
+            && (*line.offset((len - 1 as libc::c_long) as isize) as libc::c_int == '\n' as i32
+                || *line.offset((len - 1 as libc::c_long) as isize) as libc::c_int == '\r' as i32)
         {
             len -= 1;
             *line.offset(len as isize) = '\u{0}' as i32 as libc::c_char;
