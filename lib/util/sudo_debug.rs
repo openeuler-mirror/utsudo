5e9a63d6dd4ba6cd2a14f69e59159cc27356a3f2
houlifei <houlifei@uniontech.com>
2023-07-11 09:47:21 +0800

    add sudo_debug_exit_time_t_v1 function

--- lib/util/sudo_debug.rs
+++ lib/util/sudo_debug.rs
@@ -951,6 +951,29 @@ pub unsafe extern "C" fn sudo_debug_exit_ssize_t_v1(
     );
 }
 
+
+#[no_mangle]
+pub unsafe extern "C" fn sudo_debug_exit_time_t_v1(
+    func: *const libc::c_char,
+    file: *const libc::c_char,
+    line: libc::c_int,
+    subsys: libc::c_int,
+    ret: time_t,
+) {
+    sudo_debug_printf2_v1(
+        0 as *mut libc::c_char,
+        0 as *mut libc::c_char,
+        0,
+        subsys | SUDO_DEBUG_TRACE!(),
+        b"<- %s @ %s:%d := %lld\0" as *const u8 as *const libc::c_char,
+        func,
+        file,
+        line,
+        ret as libc::c_longlong,
+    );
+}
+
+
 //end
 #[no_mangle]
 pub unsafe extern "C" fn sudo_debug_execve2_v1(
