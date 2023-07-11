b16016fe6dedecbc1e9e7d54c690c808e4838cd1
bianguangze <bianguangze@uniontech.com>
2023-07-11 09:37:27 +0800

    be sure return is c_longlong

--- lib/util/strtonum.rs
+++ lib/util/strtonum.rs
@@ -8,11 +8,11 @@ mut minval: libc::c_longlong,
 mut maxval: libc::c_longlong,
 mut endp: *mut *mut libc::c_char,
 mut errstrp: *mut *const libc::c_char,        
-) {
+) -> libc::c_longlong {
 
 
 
-
+    return c_longlong;
 }
 
 
