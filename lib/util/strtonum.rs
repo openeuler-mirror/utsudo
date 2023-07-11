6848f24f2f1e766870874cdbfa5ab1f481b224dc
bianguangze <bianguangze@uniontech.com>
2023-07-11 09:33:30 +0800

    add 5 argc

--- lib/util/strtonum.rs
+++ lib/util/strtonum.rs
@@ -3,6 +3,11 @@
 
 #[no_mangle]
 pub unsafe extern "C" fn sudo_strtonumx(
+mut str: *const libc::c_char,
+mut minval: libc::c_longlong,
+mut maxval: libc::c_longlong,
+mut endp: *mut *mut libc::c_char,
+mut errstrp: *mut *const libc::c_char,        
 ) {
 
 
