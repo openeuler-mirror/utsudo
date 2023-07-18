879db3f8eceda3a39e503934d4bf0e7411a77e9e
binlingyu <binlingyu@uniontech.com>
2023-07-18 11:15:59 +0800

    add sudo_strlcpy function for strlcpy.rs file

--- lib/util/strlcpy.rs
+++ lib/util/strlcpy.rs
@@ -1,2 +1,9 @@
 #![allow(non_camel_case_types, dead_code, unused_mut, unused_variables)]
 pub type size_t = libc::c_ulong;
+
+#[no_mangle]
+fn sudo_strlcpy(
+    mut dst: *mut libc::c_char,
+    mut src: *const libc::c_char,
+    mut dsize: size_t,
+) -> size_t{}
