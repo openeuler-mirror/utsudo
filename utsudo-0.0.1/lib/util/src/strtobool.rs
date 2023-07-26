f5bb1fedf34d4c5cf1f313e7c50df9f78a2d7f30
binlingyu <binlingyu@uniontech.com>
2023-07-26 17:06:26 +0800

    Modify strtobool.rs file

--- utsudo-0.0.1/lib/util/src/strtobool.rs
+++ utsudo-0.0.1/lib/util/src/strtobool.rs
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
 #![allow(
     mutable_transmutes,
     non_camel_case_types,
