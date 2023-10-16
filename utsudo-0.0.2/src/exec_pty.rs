b632806aa62063e61c8b7d8ed971645c7f190a92
binlingyu <binlingyu@uniontech.com>
2023-10-16 16:56:11 +0800

    Modify exec_pty.rs file: add fstat function

--- utsudo-0.0.2/src/exec_pty.rs
+++ utsudo-0.0.2/src/exec_pty.rs
@@ -5,3 +5,16 @@
  */
 
 #![allow(unused_variables, unreachable_patterns, clashing_extern_declarations)]
+
+use crate::struct_macro::*;
+use utsudo_util::sudo_debug::*;
+use utsudo_util::sudo_debug_macro::*;
+use utsudo_util::*;
+
+#[inline]
+unsafe extern "C" fn fstat(mut __fd: libc::c_int, mut __statbuf: *mut stat) -> libc::c_int {
+        #[cfg(target_arch = "x86_64")]
+        return __fxstat(1, __fd, __statbuf);
+        #[cfg(not(target_arch = "x86_64"))]
+        return __fxstat(0, __fd, __statbuf);
+}
