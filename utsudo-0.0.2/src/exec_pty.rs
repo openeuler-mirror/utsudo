76392a532107af2ac69afe68c5d9f55bfe98b46a
binlingyu <binlingyu@uniontech.com>
2023-10-16 16:54:46 +0800

    add exec_pty.rs file

---
+++ utsudo-0.0.2/src/exec_pty.rs
@@ -0,0 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+#![allow(unused_variables, unreachable_patterns, clashing_extern_declarations)]
