8319ec6410ddc92388a4cff082e3dffa09a7ba0d
binlingyu <binlingyu@uniontech.com>
2023-10-16 16:50:21 +0800

    Modify get_pty.rs file : add group struct

--- utsudo-0.0.2/src/get_pty.rs
+++ utsudo-0.0.2/src/get_pty.rs
@@ -11,6 +11,15 @@ use utsudo_util::sudo_debug::*;
 use utsudo_util::sudo_debug_macro::*;
 use utsudo_util::*;
 
+#[repr(C)]
+pub struct group {
+    pub gr_name: *mut libc::c_char,
+    pub gr_passwd: *mut libc::c_char,
+    pub gr_gid: __gid_t,
+    pub gr_mem: *mut *mut libc::c_char,
+}
+
+
 #[no_mangle]
 pub unsafe extern "C" fn get_pty(
     mut master: *mut libc::c_int,
