484329cdf41c6b7ab30415b60b6a5aee5c0f2a78
binlingyu <binlingyu@uniontech.com>
2023-10-16 16:45:25 +0800

    新建get_pty.rs文件

---
+++ utsudo-0.0.2/src/get_pty.rs
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+#![allow(unused_variables)]
+
+use crate::struct_macro::*;
+use utsudo_util::sudo_debug::*;
+use utsudo_util::sudo_debug_macro::*;
+use utsudo_util::*;
