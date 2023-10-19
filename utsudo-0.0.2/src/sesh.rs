0f5c2bb09785e520251cc40fb92391f104fbd2c6
bianguangze <bianguangze@uniontech.com>
2023-10-19 15:24:13 +0800

    achieve main without main_0

--- utsudo-0.0.2/src/sesh.rs
+++ utsudo-0.0.2/src/sesh.rs
@@ -114,3 +114,36 @@ extern "C" {
     );
     fn sudo_warn_nodebug_v1(fmt: *const libc::c_char, _: ...);
 }
+
+pub fn main() {
+    let mut args: Vec<*mut libc::c_char> = Vec::new();
+    for arg in ::std::env::args() {
+        args.push(
+            (::std::ffi::CString::new(arg))
+                .expect("Failed to convert argument into CString.")
+                .into_raw(),
+        );
+    }
+    args.push(::std::ptr::null_mut());
+    let mut vars: Vec<*mut libc::c_char> = Vec::new();
+    for (var_name, var_value) in ::std::env::vars() {
+        let var: String = format!("{}={}", var_name, var_value);
+        vars.push(
+            (::std::ffi::CString::new(var))
+                .expect("Failed to convert environment variable into CString.")
+                .into_raw(),
+        );
+    }
+    vars.push(::std::ptr::null_mut());
+    unsafe {
+        ::std::process::exit(main_0(
+            (args.len() - 1) as libc::c_int,
+            args.as_mut_ptr() as *mut *mut libc::c_char,
+            vars.as_mut_ptr() as *mut *mut libc::c_char,
+        ) as i32)
+    }
+}
+
+
+
+
