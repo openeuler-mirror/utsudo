63be5a6bee006a2538e3e8a88eddf9710e3289a4
wanglujun <wanglujun@uniontech.com>
2023-10-24 09:25:46 +0800

    add some struct for common

--- utsudo-0.0.2/src/struct_macro.rs
+++ utsudo-0.0.2/src/struct_macro.rs
@@ -298,3 +298,273 @@ pub type __nlink_t = libc::c_ulong;
 pub type __nlink_t = libc::c_uint;
 pub type __id_t = libc::c_uint;
 pub type id_t = __id_t;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct _IO_wide_data {
+    _unused: [u8; 0],
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct _IO_codecvt {
+    _unused: [u8; 0],
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct _IO_marker {
+    _unused: [u8; 0],
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct passwd {
+    pub pw_name: *mut libc::c_char,
+    pub pw_passwd: *mut libc::c_char,
+    pub pw_uid: __uid_t,
+    pub pw_gid: __gid_t,
+    pub pw_gecos: *mut libc::c_char,
+    pub pw_dir: *mut libc::c_char,
+    pub pw_shell: *mut libc::c_char,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct pollfd {
+    pub fd: libc::c_int,
+    pub events: libc::c_short,
+    pub revents: libc::c_short,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct __spawn_action {
+    pub _address: u8,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct sudo_conv_message {
+    pub msg_type: libc::c_int,
+    pub timeout: libc::c_int,
+    pub msg: *const libc::c_char,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct sudo_conv_reply {
+    pub reply: *mut libc::c_char,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct sudo_conv_callback {
+    pub version: libc::c_uint,
+    pub closure: *mut libc::c_void,
+    pub on_suspend: sudo_conv_callback_fn_t,
+    pub on_resume: sudo_conv_callback_fn_t,
+}
+pub type sudo_conv_callback_fn_t =
+    Option<unsafe extern "C" fn(libc::c_int, *mut libc::c_void) -> libc::c_int>;
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct _IO_FILE {
+    pub _flags: libc::c_int,
+    pub _IO_read_ptr: *mut libc::c_char,
+    pub _IO_read_end: *mut libc::c_char,
+    pub _IO_read_base: *mut libc::c_char,
+    pub _IO_write_base: *mut libc::c_char,
+    pub _IO_write_ptr: *mut libc::c_char,
+    pub _IO_write_end: *mut libc::c_char,
+    pub _IO_buf_base: *mut libc::c_char,
+    pub _IO_buf_end: *mut libc::c_char,
+    pub _IO_save_base: *mut libc::c_char,
+    pub _IO_backup_base: *mut libc::c_char,
+    pub _IO_save_end: *mut libc::c_char,
+    pub _markers: *mut _IO_marker,
+    pub _chain: *mut _IO_FILE,
+    pub _fileno: libc::c_int,
+    pub _flags2: libc::c_int,
+    pub _old_offset: __off_t,
+    pub _cur_column: libc::c_ushort,
+    pub _vtable_offset: libc::c_schar,
+    pub _shortbuf: [libc::c_char; 1],
+    pub _lock: *mut libc::c_void,
+    pub _offset: __off64_t,
+    pub _codecvt: *mut _IO_codecvt,
+    pub _wide_data: *mut _IO_wide_data,
+    pub _freeres_list: *mut _IO_FILE,
+    pub _freeres_buf: *mut libc::c_void,
+    pub __pad5: size_t,
+    pub _mode: libc::c_int,
+    pub _unused2: [libc::c_char; 20],
+}
+pub type _IO_lock_t = ();
+pub type FILE = _IO_FILE;
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct command_status {
+    pub type_0: libc::c_int,
+    pub val: libc::c_int,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct siginfo_t {
+    pub si_signo: libc::c_int,
+    pub si_errno: libc::c_int,
+    pub si_code: libc::c_int,
+    pub __pad0: libc::c_int,
+    pub _sifields: siginfo_t_u,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub union siginfo_t_u {
+    pub _pad: [libc::c_int; 28],
+    pub _kill: _kill_s,
+    pub _timer: _timer_s,
+    pub _rt: _rt_s,
+    pub _sigchld: _sigchld_s,
+    pub _sigfault: _sigfault_s,
+    pub _sigpoll: _sigpoll_s,
+    pub _sigsys: _sigsys_s,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct _kill_s {
+    pub si_pid: __pid_t,
+    pub si_uid: __uid_t,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct _timer_s {
+    pub si_tid: libc::c_int,
+    pub si_overrun: libc::c_int,
+    pub si_sigval: __sigval_t,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub union sigval {
+    pub sival_int: libc::c_int,
+    pub sival_ptr: *mut libc::c_void,
+}
+pub type __sigval_t = sigval;
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct _rt_s {
+    pub si_pid: __pid_t,
+    pub si_uid: __uid_t,
+    pub si_sigval: __sigval_t,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct _sigchld_s {
+    pub si_pid: __pid_t,
+    pub si_uid: __uid_t,
+    pub si_status: libc::c_int,
+    pub si_utime: __clock_t,
+    pub si_stime: __clock_t,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct _sigfault_s {
+    pub si_addr: *mut libc::c_void,
+    pub si_addr_lsb: libc::c_short,
+    pub _bounds: _bounds_u,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub union _bounds_u {
+    pub _addr_bnd: _addr_bnd_s,
+    pub _pkey: __uint32_t,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct _addr_bnd_s {
+    pub _lower: *mut libc::c_void,
+    pub _upper: *mut libc::c_void,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct _sigpoll_s {
+    pub si_band: libc::c_long,
+    pub si_fd: libc::c_int,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct _sigsys_s {
+    pub _call_addr: *mut libc::c_void,
+    pub _syscall: libc::c_int,
+    pub _arch: libc::c_uint,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct sudo_event_base {
+    pub events: sudo_event_list,
+    pub active: sudo_event_list,
+    pub timeouts: sudo_event_list,
+    pub signal_event: sudo_event,
+    pub signals: [sudo_event_list; NSIG as usize],
+    pub orig_handlers: [*mut sigaction; NSIG as usize],
+    pub siginfo: [*mut siginfo_t; NSIG as usize],
+    pub signal_pending: [sig_atomic_t; NSIG as usize],
+    pub signal_caught: sig_atomic_t,
+    pub num_handlers: libc::c_int,
+    pub signal_pipe: [libc::c_int; 2],
+    pub pfds: *mut pollfd,
+    pub pfd_max: libc::c_int,
+    pub pfd_high: libc::c_int,
+    pub pfd_free: libc::c_int,
+    pub flags: libc::c_uint,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct sudo_event_list {
+    pub tqh_first: *mut sudo_event,
+    pub tqh_last: *mut *mut sudo_event,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct sudo_event {
+    pub entries: TAILQ_ENTRY_sudo_event,
+    pub active_entries: TAILQ_ENTRY_active_entries,
+    pub timeouts_entries: TAILQ_ENTRY_timeouts_entries,
+    pub base: *mut sudo_event_base,
+    pub fd: libc::c_int,
+    pub events: libc::c_short,
+    pub revents: libc::c_short,
+    pub flags: libc::c_short,
+    pub pfd_idx: libc::c_short,
+    pub callback: sudo_ev_callback_t,
+    pub timeout: timespec,
+    pub closure: *mut libc::c_void,
+}
+pub type sudo_ev_callback_t =
+    Option<unsafe extern "C" fn(libc::c_int, libc::c_int, *mut libc::c_void) -> ()>;
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct TAILQ_ENTRY_sudo_event {
+    pub tqe_next: *mut sudo_event,
+    pub tqe_prev: *mut *mut sudo_event,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct TAILQ_ENTRY_active_entries {
+    pub tqe_next: *mut sudo_event,
+    pub tqe_prev: *mut *mut sudo_event,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct TAILQ_ENTRY_timeouts_entries {
+    pub tqe_next: *mut sudo_event,
+    pub tqe_prev: *mut *mut sudo_event,
+}
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct timespec {
+    pub tv_sec: __time_t,
+    pub tv_nsec: __syscall_slong_t,
+}
