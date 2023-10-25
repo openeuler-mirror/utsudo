2d5d94e2f3505941cc8293104e39cc09a3dfa7c2
bianguangze <bianguangze@uniontech.com>
2023-10-25 10:11:30 +0800

    some funcs depend from libc

--- utsudo-0.0.2/src/sudo_edit.rs
+++ utsudo-0.0.2/src/sudo_edit.rs
@@ -54,3 +54,113 @@ macro_rules! sudo_timespeccmp {
     })
     }};
 }
+
+extern "C" {
+    fn umask(__mask: __mode_t) -> __mode_t;
+    fn utimensat(
+        __fd: libc::c_int,
+        __path: *const libc::c_char,
+        __times: *const timespec,
+        __flags: libc::c_int,
+    ) -> libc::c_int;
+    fn futimens(__fd: libc::c_int, __times: *const timespec) -> libc::c_int;
+    fn __fxstat(__ver: libc::c_int, __fildes: libc::c_int, __stat_buf: *mut stat) -> libc::c_int;
+    fn __xstat(
+        __ver: libc::c_int,
+        __filename: *const libc::c_char,
+        __stat_buf: *mut stat,
+    ) -> libc::c_int;
+    fn waitpid(__pid: __pid_t, __stat_loc: *mut libc::c_int, __options: libc::c_int) -> __pid_t;
+    fn asprintf(__ptr: *mut *mut libc::c_char, __fmt: *const libc::c_char, _: ...) -> libc::c_int;
+    fn calloc(_: libc::c_ulong, _: libc::c_ulong) -> *mut libc::c_void;
+    fn reallocarray(__ptr: *mut libc::c_void, __nmemb: size_t, __size: size_t)
+        -> *mut libc::c_void;
+    fn free(__ptr: *mut libc::c_void);
+    fn mkstemps(__template: *mut libc::c_char, __suffixlen: libc::c_int) -> libc::c_int;
+    fn memcpy(_: *mut libc::c_void, _: *const libc::c_void, _: libc::c_ulong) -> *mut libc::c_void;
+    fn memset(_: *mut libc::c_void, _: libc::c_int, _: libc::c_ulong) -> *mut libc::c_void;
+    fn strcmp(_: *const libc::c_char, _: *const libc::c_char) -> libc::c_int;
+    fn strchr(_: *const libc::c_char, _: libc::c_int) -> *mut libc::c_char;
+    fn strrchr(_: *const libc::c_char, _: libc::c_int) -> *mut libc::c_char;
+    fn strlen(_: *const libc::c_char) -> libc::c_ulong;
+    fn faccessat(
+        __fd: libc::c_int,
+        __file: *const libc::c_char,
+        __type: libc::c_int,
+        __flag: libc::c_int,
+    ) -> libc::c_int;
+    fn close(__fd: libc::c_int) -> libc::c_int;
+    fn fchown(__fd: libc::c_int, __owner: __uid_t, __group: __gid_t) -> libc::c_int;
+    fn execve(
+        __path: *const libc::c_char,
+        __argv: *const *mut libc::c_char,
+        __envp: *const *mut libc::c_char,
+    ) -> libc::c_int;
+    fn _exit(_: libc::c_int) -> !;
+    fn setuid(__uid: __uid_t) -> libc::c_int;
+    fn seteuid(__uid: __uid_t) -> libc::c_int;
+    fn setegid(__gid: __gid_t) -> libc::c_int;
+    fn unlink(__name: *const libc::c_char) -> libc::c_int;
+    fn __errno_location() -> *mut libc::c_int;
+    fn fcntl(__fd: libc::c_int, __cmd: libc::c_int, _: ...) -> libc::c_int;
+    fn open(__file: *const libc::c_char, __oflag: libc::c_int, _: ...) -> libc::c_int;
+    fn openat(
+        __fd: libc::c_int,
+        __file: *const libc::c_char,
+        __oflag: libc::c_int,
+        _: ...
+    ) -> libc::c_int;
+    fn sudo_gettime_real_v1(ts: *mut timespec) -> libc::c_int;
+    fn sudo_warn_gettext_v1(
+        domainname: *const libc::c_char,
+        msgid: *const libc::c_char,
+    ) -> *mut libc::c_char;
+    fn sudo_conf_sesh_path_v1() -> *const libc::c_char;
+    fn sudo_debug_exit_bool_v1(
+        func: *const libc::c_char,
+        file: *const libc::c_char,
+        line: libc::c_int,
+        subsys: libc::c_int,
+        ret: bool,
+    );
+    fn sudo_debug_fork_v1() -> pid_t;
+    fn sudo_debug_printf2_v1(
+        func: *const libc::c_char,
+        file: *const libc::c_char,
+        line: libc::c_int,
+        level: libc::c_int,
+        fmt: *const libc::c_char,
+        _: ...
+    );
+    fn sudo_setgroups_v1(ngids: libc::c_int, gids: *const gid_t) -> libc::c_int;
+    fn run_command(details: *mut command_details) -> libc::c_int;
+    static mut user_details: user_details;
+    fn sudo_strlcpy(dst: *mut libc::c_char, src: *const libc::c_char, siz: size_t) -> size_t;
+    fn selinux_setcon() -> libc::c_int;
+    fn selinux_setup(
+        role: *const libc::c_char,
+        type_0: *const libc::c_char,
+        ttyn: *const libc::c_char,
+        ttyfd: libc::c_int,
+        label_tty: bool,
+    ) -> libc::c_int;
+    fn sudo_fatal_nodebug_v1(fmt: *const libc::c_char, _: ...) -> !;
+    fn sudo_fatalx_nodebug_v1(fmt: *const libc::c_char, _: ...) -> !;
+    fn sudo_warn_nodebug_v1(fmt: *const libc::c_char, _: ...);
+    fn sudo_warnx_nodebug_v1(fmt: *const libc::c_char, _: ...);
+    fn sudo_copy_file(
+        src: *const libc::c_char,
+        src_fd: libc::c_int,
+        src_len: off_t,
+        dst: *const libc::c_char,
+        dst_fd: libc::c_int,
+        dst_len: off_t,
+    ) -> libc::c_int;
+    fn sudo_check_temp_file(
+        tfd: libc::c_int,
+        tname: *const libc::c_char,
+        uid: uid_t,
+        sb: *mut stat,
+    ) -> bool;
+}
+
