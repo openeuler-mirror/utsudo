88a5a651b2c93aba93e5e43265731fab0a067a14
wanglujun <wanglujun@uniontech.com>
2023-11-17 09:50:25 +0800

    update src/lib.rs

--- utsudo-0.0.2/src/lib.rs
+++ utsudo-0.0.2/src/lib.rs
@@ -1,23 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
 
-#![warn(missing_docs, unreachable_pub)]
-pub mod duration;
-#[macro_use]
-pub mod macros;
-pub mod num;
-pub mod option;
-pub mod result;
-pub mod str;
-pub mod sync;
-pub mod vec;
-/// A "prelude" module which re-exports all the extension traits for the simple library usage.
-pub mod prelude {
-    pub use crate::{
-        duration::*,
-        num::{float_convert::*, integer::*},
-        option::*,
-        result::*,
-        str::*,
-        sync::{mutex::*, rw_lock::*},
-        vec::*,
-    };
-}
+mod struct_macro;
+mod sudo_noexec;
