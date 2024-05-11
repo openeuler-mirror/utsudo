27dabe6365a39b21120b5e209bb5491824c7a1c5
bianguangze <bianguangze@uniontech.com>
2024-05-11 13:55:53 +0800

    add common

---
+++ utsudo-0.0.3/plugins/group_file/src/common.rs
@@ -0,0 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+////////////  调用其它模块中接口
+pub use utsudo_util::common::*;
+pub use utsudo_util::*;
