99575825cf9276b12f990078b24b22b1a7ca0405
bianguangze <bianguangze@uniontech.com>
2024-05-13 15:19:35 +0800

    add fn main of plugin_test.rs

--- utsudo-0.0.3/plugins/group_file/src/plugin_test.rs
+++ utsudo-0.0.3/plugins/group_file/src/plugin_test.rs
@@ -32,3 +32,29 @@ use crate::common::*;
 #[link(name = "utsudo_util")]
 #[link(name = "util_variadic")]
 #[link(name = "group_file_variadic")]
+
+
+
+pub fn main() {
+    let mut args: Vec<*mut libc::c_char> = Vec::new();
+    for arg in ::std::env::args() {
+        args.push(
+            (::std::ffi::CString::new(arg))
+                .expect("Failed to convert argument into CString.")
+                .into_raw(),
+        );
+    }
+    args.push(::std::ptr::null_mut());
+    unsafe {
+        ::std::process::exit(main_0(
+            (args.len() - 1) as libc::c_int,
+            args.as_mut_ptr() as *mut *mut libc::c_char,
+        ) as i32)
+    }
+}
+
+
+
+
+
+
