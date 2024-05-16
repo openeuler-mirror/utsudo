bb24d386ee3e365ba19af4a0c263e7e281c8ff4f
bianguangze <bianguangze@uniontech.com>
2024-05-16 11:07:38 +0800

    file alias.rs add copyright and attributes

---
+++ utsudo-0.0.3/plugins/sudoers/src/alias.rs
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+#![allow(
+    dead_code,
+    mutable_transmutes,
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
+    unused_assignments,
+    unused_mut,
+    unreachable_code,
+    non_snake_case,
+    unreachable_patterns,
+    unused_variables,
+    clashing_extern_declarations
+)]
