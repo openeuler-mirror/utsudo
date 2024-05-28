48522663560e43ed96db012e2d2c2892bb9d90c9
bianguangze <bianguangze@uniontech.com>
2024-05-28 09:06:18 +0800

    add libc_fns

---
+++ utsudo-0.0.3/plugins/sudoers/src/find_path.rs
@@ -0,0 +1,71 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+#![allow(
+    dead_code,
+    mutable_transmutes,
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
+    unused_assignments,
+    unused_mut,
+    clashing_extern_declarations
+)]
+use crate::common::*;
+extern "C" {
+    fn snprintf(
+        _: *mut libc::c_char,
+        _: libc::c_ulong,
+        _: *const libc::c_char,
+        _: ...
+    ) -> libc::c_int;
+    fn strcmp(_: *const libc::c_char, _: *const libc::c_char) -> libc::c_int;
+    fn strdup(_: *const libc::c_char) -> *mut libc::c_char;
+    fn strchr(_: *const libc::c_char, _: libc::c_int) -> *mut libc::c_char;
+    fn strrchr(_: *const libc::c_char, _: libc::c_int) -> *mut libc::c_char;
+    fn strlen(_: *const libc::c_char) -> libc::c_ulong;
+    fn __errno_location() -> *mut libc::c_int;
+    fn sudo_strlcpy(dst: *mut libc::c_char, src: *const libc::c_char, siz: size_t) -> size_t;
+    fn sudo_strsplit_v1(
+        str: *const libc::c_char,
+        endstr: *const libc::c_char,
+        sep: *const libc::c_char,
+        last: *mut *const libc::c_char,
+    ) -> *const libc::c_char;
+    fn sudo_debug_enter_v1(
+        func: *const libc::c_char,
+        file: *const libc::c_char,
+        line: libc::c_int,
+        subsys: libc::c_int,
+    );
+    fn sudo_debug_exit_bool_v1(
+        func: *const libc::c_char,
+        file: *const libc::c_char,
+        line: libc::c_int,
+        subsys: libc::c_int,
+        ret: bool,
+    );
+    fn sudo_debug_exit_int_v1(
+        func: *const libc::c_char,
+        file: *const libc::c_char,
+        line: libc::c_int,
+        subsys: libc::c_int,
+        ret: libc::c_int,
+    );
+    static mut sudoers_subsystem_ids: [libc::c_uint; 0];
+    fn sudo_goodpath(path: *const libc::c_char, sbp: *mut stat) -> bool;
+}
+pub type __dev_t = libc::c_ulong;
+pub type __uid_t = libc::c_uint;
+pub type __gid_t = libc::c_uint;
+pub type __ino_t = libc::c_ulong;
+pub type __mode_t = libc::c_uint;
+pub type __off_t = libc::c_long;
+pub type __time_t = libc::c_long;
+pub type __blkcnt_t = libc::c_long;
+pub type __ssize_t = libc::c_long;
+pub type __syscall_slong_t = libc::c_long;
+pub type ssize_t = __ssize_t;
+pub type size_t = libc::c_ulong;
