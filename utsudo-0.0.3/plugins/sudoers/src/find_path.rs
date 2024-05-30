db2c62846eb2941d785b064fe1dabf86c8123299
bianguangze <bianguangze@uniontech.com>
2024-05-30 09:08:32 +0800

    finish file find_path.rs

--- utsudo-0.0.3/plugins/sudoers/src/find_path.rs
+++ utsudo-0.0.3/plugins/sudoers/src/find_path.rs
@@ -166,3 +166,200 @@ unsafe extern "C" fn cmnd_allowed(
     return sudo_debug_ret_3;
 }
 
+#[no_mangle]
+pub unsafe extern "C" fn find_path(
+    mut infile: *const libc::c_char,
+    mut outfile: *mut *mut libc::c_char,
+    mut sbp: *mut stat,
+    mut path: *const libc::c_char,
+    mut ignore_dot: libc::c_int,
+    mut whitelist: *const *mut libc::c_char,
+) -> libc::c_int {
+    let mut command: [libc::c_char; 4096] = [0; 4096];
+    let mut cp: *const libc::c_char = 0 as *const libc::c_char;
+    let mut ep: *const libc::c_char = 0 as *const libc::c_char;
+    let mut pathend: *const libc::c_char = 0 as *const libc::c_char;
+    let mut found: bool = 0 as libc::c_int != 0;
+    let mut checkdot: bool = 0 as libc::c_int != 0;
+    let mut len: libc::c_int = 0;
+    let sudo_debug_subsys_tmp: libc::c_int = *sudoers_subsystem_ids
+        .as_mut_ptr()
+        .offset(17 as libc::c_int as isize)
+        as libc::c_int;
+    sudo_debug_enter_v1(
+        (*::core::mem::transmute::<&[u8; 10], &[libc::c_char; 10]>(b"find_path\0")).as_ptr(),
+        b"find_path.c\0" as *const u8 as *const libc::c_char,
+        109 as libc::c_int,
+        sudo_debug_subsys_tmp,
+    );
+    if !(strchr(infile, '/' as i32)).is_null() {
+        if sudo_strlcpy(
+            command.as_mut_ptr(),
+            infile,
+            ::core::mem::size_of::<[libc::c_char; 4096]>() as libc::c_ulong,
+        ) >= ::core::mem::size_of::<[libc::c_char; 4096]>() as libc::c_ulong
+        {
+            *__errno_location() = 36 as libc::c_int;
+            let mut sudo_debug_ret: libc::c_int = 3 as libc::c_int;
+            sudo_debug_exit_int_v1(
+                (*::core::mem::transmute::<&[u8; 10], &[libc::c_char; 10]>(b"find_path\0"))
+                    .as_ptr(),
+                b"find_path.c\0" as *const u8 as *const libc::c_char,
+                118 as libc::c_int,
+                sudo_debug_subsys_tmp,
+                sudo_debug_ret,
+            );
+            return sudo_debug_ret;
+        }
+        found = cmnd_allowed(
+            command.as_mut_ptr(),
+            ::core::mem::size_of::<[libc::c_char; 4096]>() as libc::c_ulong,
+            sbp,
+            whitelist,
+        );
+    } else {
+        if path.is_null() {
+            let mut sudo_debug_ret_0: libc::c_int = 1 as libc::c_int;
+            sudo_debug_exit_int_v1(
+                (*::core::mem::transmute::<&[u8; 10], &[libc::c_char; 10]>(b"find_path\0"))
+                    .as_ptr(),
+                b"find_path.c\0" as *const u8 as *const libc::c_char,
+                125 as libc::c_int,
+                sudo_debug_subsys_tmp,
+                sudo_debug_ret_0,
+            );
+            return sudo_debug_ret_0;
+        }
+        pathend = path.offset(strlen(path) as isize);
+        cp = sudo_strsplit_v1(
+            path,
+            pathend,
+            b":\0" as *const u8 as *const libc::c_char,
+            &mut ep,
+        );
+        while !cp.is_null() {
+            if cp == ep
+                || *cp as libc::c_int == '.' as i32 && cp.offset(1 as libc::c_int as isize) == ep
+            {
+                checkdot = 1 as libc::c_int != 0;
+            } else {
+                len = snprintf(
+                    command.as_mut_ptr(),
+                    ::core::mem::size_of::<[libc::c_char; 4096]>() as libc::c_ulong,
+                    b"%.*s/%s\0" as *const u8 as *const libc::c_char,
+                    ep.offset_from(cp) as libc::c_long as libc::c_int,
+                    cp,
+                    infile,
+                );
+                if len < 0 as libc::c_int
+                    || len as libc::c_long
+                        >= ::core::mem::size_of::<[libc::c_char; 4096]>() as libc::c_ulong
+                            as ssize_t
+                {
+                    *__errno_location() = 36 as libc::c_int;
+                    let mut sudo_debug_ret_1: libc::c_int = 3 as libc::c_int;
+                    sudo_debug_exit_int_v1(
+                        (*::core::mem::transmute::<&[u8; 10], &[libc::c_char; 10]>(b"find_path\0"))
+                            .as_ptr(),
+                        b"find_path.c\0" as *const u8 as *const libc::c_char,
+                        147 as libc::c_int,
+                        sudo_debug_subsys_tmp,
+                        sudo_debug_ret_1,
+                    );
+                    return sudo_debug_ret_1;
+                }
+                found = cmnd_allowed(
+                    command.as_mut_ptr(),
+                    ::core::mem::size_of::<[libc::c_char; 4096]>() as libc::c_ulong,
+                    sbp,
+                    whitelist,
+                );
+                if found {
+                    break;
+                }
+            }
+            cp = sudo_strsplit_v1(
+                0 as *const libc::c_char,
+                pathend,
+                b":\0" as *const u8 as *const libc::c_char,
+                &mut ep,
+            );
+        }
+        if !found && checkdot as libc::c_int != 0 {
+            len = snprintf(
+                command.as_mut_ptr(),
+                ::core::mem::size_of::<[libc::c_char; 4096]>() as libc::c_ulong,
+                b"./%s\0" as *const u8 as *const libc::c_char,
+                infile,
+            );
+            if len < 0 as libc::c_int
+                || len as libc::c_long
+                    >= ::core::mem::size_of::<[libc::c_char; 4096]>() as libc::c_ulong as ssize_t
+            {
+                *__errno_location() = 36 as libc::c_int;
+                let mut sudo_debug_ret_2: libc::c_int = 3 as libc::c_int;
+                sudo_debug_exit_int_v1(
+                    (*::core::mem::transmute::<&[u8; 10], &[libc::c_char; 10]>(b"find_path\0"))
+                        .as_ptr(),
+                    b"find_path.c\0" as *const u8 as *const libc::c_char,
+                    161 as libc::c_int,
+                    sudo_debug_subsys_tmp,
+                    sudo_debug_ret_2,
+                );
+                return sudo_debug_ret_2;
+            }
+            found = cmnd_allowed(
+                command.as_mut_ptr(),
+                ::core::mem::size_of::<[libc::c_char; 4096]>() as libc::c_ulong,
+                sbp,
+                whitelist,
+            );
+            if found as libc::c_int != 0 && ignore_dot != 0 {
+                let mut sudo_debug_ret_3: libc::c_int = 2 as libc::c_int;
+                sudo_debug_exit_int_v1(
+                    (*::core::mem::transmute::<&[u8; 10], &[libc::c_char; 10]>(b"find_path\0"))
+                        .as_ptr(),
+                    b"find_path.c\0" as *const u8 as *const libc::c_char,
+                    165 as libc::c_int,
+                    sudo_debug_subsys_tmp,
+                    sudo_debug_ret_3,
+                );
+                return sudo_debug_ret_3;
+            }
+        }
+    }
+    if found {
+        *outfile = strdup(command.as_mut_ptr());
+        if (*outfile).is_null() {
+            let mut sudo_debug_ret_4: libc::c_int = 3 as libc::c_int;
+            sudo_debug_exit_int_v1(
+                (*::core::mem::transmute::<&[u8; 10], &[libc::c_char; 10]>(b"find_path\0"))
+                    .as_ptr(),
+                b"find_path.c\0" as *const u8 as *const libc::c_char,
+                171 as libc::c_int,
+                sudo_debug_subsys_tmp,
+                sudo_debug_ret_4,
+            );
+            return sudo_debug_ret_4;
+        }
+        let mut sudo_debug_ret_5: libc::c_int = 0 as libc::c_int;
+        sudo_debug_exit_int_v1(
+            (*::core::mem::transmute::<&[u8; 10], &[libc::c_char; 10]>(b"find_path\0")).as_ptr(),
+            b"find_path.c\0" as *const u8 as *const libc::c_char,
+            172 as libc::c_int,
+            sudo_debug_subsys_tmp,
+            sudo_debug_ret_5,
+        );
+        return sudo_debug_ret_5;
+    }
+    let mut sudo_debug_ret_6: libc::c_int = 1 as libc::c_int;
+    sudo_debug_exit_int_v1(
+        (*::core::mem::transmute::<&[u8; 10], &[libc::c_char; 10]>(b"find_path\0")).as_ptr(),
+        b"find_path.c\0" as *const u8 as *const libc::c_char,
+        174 as libc::c_int,
+        sudo_debug_subsys_tmp,
+        sudo_debug_ret_6,
+    );
+    return sudo_debug_ret_6;
+}
+
