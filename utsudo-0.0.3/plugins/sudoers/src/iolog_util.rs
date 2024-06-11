b7be2a114ce7521adc81dbbaa780ba21de52e90f
binlingyu <binlingyu@uniontech.com>
2024-06-11 17:26:45 +0800

    add extern C function for iolog_util.rs file

--- utsudo-0.0.3/plugins/sudoers/src/iolog_util.rs
+++ utsudo-0.0.3/plugins/sudoers/src/iolog_util.rs
@@ -14,3 +14,79 @@
     unused_mut
 )]
 use crate::common::*;
+
+extern "C" {
+    fn fclose(__stream: *mut FILE) -> libc::c_int;
+    fn fopen(_: *const libc::c_char, _: *const libc::c_char) -> *mut FILE;
+    fn getdelim(
+        __lineptr: *mut *mut libc::c_char,
+        __n: *mut size_t,
+        __delimiter: libc::c_int,
+        __stream: *mut FILE,
+    ) -> __ssize_t;
+    fn strtoul(_: *const libc::c_char, _: *mut *mut libc::c_char, _: libc::c_int) -> libc::c_ulong;
+    fn calloc(_: libc::c_ulong, _: libc::c_ulong) -> *mut libc::c_void;
+    fn free(_: *mut libc::c_void);
+    fn memcpy(_: *mut libc::c_void, _: *const libc::c_void, _: libc::c_ulong) -> *mut libc::c_void;
+    fn strdup(_: *const libc::c_char) -> *mut libc::c_char;
+    fn strndup(_: *const libc::c_char, _: libc::c_ulong) -> *mut libc::c_char;
+    fn strchr(_: *const libc::c_char, _: libc::c_int) -> *mut libc::c_char;
+    fn strcspn(_: *const libc::c_char, _: *const libc::c_char) -> libc::c_ulong;
+    fn __ctype_b_loc() -> *mut *const libc::c_ushort;
+    fn __errno_location() -> *mut libc::c_int;
+    fn sudo_str2sig(signame: *const libc::c_char, signum: *mut libc::c_int) -> libc::c_int;
+    fn sudo_warn_gettext_v1(
+        domainname: *const libc::c_char,
+        msgid: *const libc::c_char,
+    ) -> *mut libc::c_char;
+    fn sudo_fatalx_nodebug_v1(fmt: *const libc::c_char, _: ...) -> !;
+    fn sudo_warn_nodebug_v1(fmt: *const libc::c_char, _: ...);
+    fn sudo_debug_enter_v1(
+        func: *const libc::c_char,
+        file: *const libc::c_char,
+        line: libc::c_int,
+        subsys: libc::c_int,
+    );
+    fn sudo_debug_exit_v1(
+        func: *const libc::c_char,
+        file: *const libc::c_char,
+        line: libc::c_int,
+        subsys: libc::c_int,
+    );
+    fn sudo_debug_exit_bool_v1(
+        func: *const libc::c_char,
+        file: *const libc::c_char,
+        line: libc::c_int,
+        subsys: libc::c_int,
+        ret: bool,
+    );
+    fn sudo_debug_exit_ptr_v1(
+        func: *const libc::c_char,
+        file: *const libc::c_char,
+        line: libc::c_int,
+        subsys: libc::c_int,
+        ret: *const libc::c_void,
+    );
+    fn sudo_debug_exit_str_v1(
+        func: *const libc::c_char,
+        file: *const libc::c_char,
+        line: libc::c_int,
+        subsys: libc::c_int,
+        ret: *const libc::c_char,
+    );
+    fn sudo_debug_printf2_v1(
+        func: *const libc::c_char,
+        file: *const libc::c_char,
+        line: libc::c_int,
+        level: libc::c_int,
+        fmt: *const libc::c_char,
+        _: ...
+    );
+    fn sudo_strtonum(
+        _: *const libc::c_char,
+        _: libc::c_longlong,
+        _: libc::c_longlong,
+        _: *mut *const libc::c_char,
+    ) -> libc::c_longlong;
+}
+
